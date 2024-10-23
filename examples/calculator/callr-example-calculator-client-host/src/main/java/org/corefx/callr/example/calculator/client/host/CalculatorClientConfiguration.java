38d8f036eabae47ee0a918192213ca5e9567c1e3
Vladimir Granitsky <vlado@granitsky.net>
2024-10-23 18:15:33 +0300

    Added more examples in Calculator example.

--- examples/calculator/callr-example-calculator-client-host/src/main/java/org/corefx/callr/example/calculator/client/host/CalculatorClientConfiguration.java
+++ examples/calculator/callr-example-calculator-client-host/src/main/java/org/corefx/callr/example/calculator/client/host/CalculatorClientConfiguration.java
@@ -1,16 +1,13 @@
 package org.corefx.callr.example.calculator.client.host;
 
-import lombok.AllArgsConstructor;
 import org.corefx.callr.client.CallRClient;
 import org.corefx.callr.example.calculator.Calculator;
 import org.corefx.callr.example.calculator.client.CalculatorServiceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AllArgsConstructor
 public class CalculatorClientConfiguration {
 
 
--- examples/calculator/callr-example-calculator-client-host/src/main/java/org/corefx/callr/example/calculator/client/host/CalculatorClientHost.java
+++ examples/calculator/callr-example-calculator-client-host/src/main/java/org/corefx/callr/example/calculator/client/host/CalculatorClientHost.java
@@ -9,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.MessageFormat;
+
 @AllArgsConstructor
 @Slf4j
 @SpringBootApplication
@@ -25,16 +27,37 @@ public class CalculatorClientHost implements CommandLineRunner {
 	@SneakyThrows
 	@Override
 	public void run(String... args) {
-		// calculator.ping();
+
+		calculator.ping();
+
 		int a = 1;
-		int b = 0;
+		int b = 2;
+		int c = calculator.add(a, b);
+		System.out.println(MessageFormat.format("{0} + {1} = {2}", a, b, c));
+
+		a = 10;
+		b = 5;
+		c = calculator.div(a, b);
+		System.out.println(MessageFormat.format("{0} / {1} = {2}", a, b, c));
+
+		double da = 6.66;
+		double db = 3.33;
+		double dc = calculator.div(da, db);
+		System.out.println(MessageFormat.format("{0} / {1} = {2}", da, db, dc));
+
+		db = 0.00;
+		dc = calculator.div(da, db);
+		System.out.println(MessageFormat.format("{0} / {1} = {2}", da, db, dc));
+
+		b = 0;
 		try {
-			double c = calculator.div(a, b);
+			c = calculator.div(a, b);
 			System.out.println(c);
 		}
 		catch(Exception ex) {
 			log.error(ex.getMessage(), ex);
 		}
+		System.out.println(MessageFormat.format("{0} / {1} => java.lang.ArithmeticException: / by zero", a, b));
 	}
 
 }
