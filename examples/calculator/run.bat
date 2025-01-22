1f37b8a44cd734af19191bc9cb2f53695e86694b
Vladimir Granitsky <vlado@granitsky.net>
2025-01-22 19:36:04 +0200

    Fixed run.bat in calculator example.

--- examples/calculator/run.bat
+++ examples/calculator/run.bat
@@ -1,5 +1,5 @@
-start /k java -jar callr-example-calculator-hub-host\target\callr-example-calculator-hub-host-1.0.0-SNAPSHOT.jar
+start /k java -jar calculator-hub-host\target\calculator-hub-host-1.0.0-SNAPSHOT.jar
 timeout 5 > NUL
-start /k java -jar callr-example-calculator-service-host\target\callr-example-calculator-service-host-1.0.0-SNAPSHOT.jar
+start /k java -jar calculator-service-host\target\calculator-service-host-1.0.0-SNAPSHOT.jar
 timeout 5 > NUL
-start /k java -jar callr-example-calculator-client-host\target\callr-example-calculator-client-host-1.0.0-SNAPSHOT.jar
+start /k java -jar calculator-client-host\target\calculator-client-host-1.0.0-SNAPSHOT.jar
